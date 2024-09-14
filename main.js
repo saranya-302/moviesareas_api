4aadeb94bd9d40610c9fb657b69a60b9b8be9ee9
saranya-302 <saranyag3026@gmail.com>
2024-09-14 15:16:30 +0530

    Route-Structure

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ const PORT = 3026;
 app.get('/',(req,res)=>{
     res.json({msg:"Hello Students!"});
 })
+
+//CRUD Functionality of movies
+//R - For Reading
+app.get("/movies",()=>{});
+//C - For creating movies
+app.post("/movies",()=>{});
+//U - For Updating movies
+app.put("/movies",()=>{});
+//D - For Deleting movies
+app.delete("/moives",()=>{});
 app.listen(PORT,()=>{
     console.log(`The server is running at http://localhost:${PORT}`);
     
